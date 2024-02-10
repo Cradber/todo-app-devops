9cdde94717df4e7fed3592b2669144c3c4816476
Yankiel Pacheco <yankiel.pacheco@between.tech>
2024-02-10 15:15:40 +0100

    Initialize API URL in the constructor

--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,9 +1,18 @@
 from sys import stderr
 
+import requests
+
 
 class ApiService:
     def __init__(self):
-        pass
+        self.api = "https://jsonplaceholder.typicode.com/todos/"
+
+    def get_all_todos(self):
+        response = requests.get(self.api)
+        if response.ok:
+            return response.json()
+        else:
+            response.raise_for_status()
 
     def run(self):
         print('Running ApiService', file=stderr)
