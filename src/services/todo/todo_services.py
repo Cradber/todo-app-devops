a465c40645665c549290c10f61439c6c4df21f21
yankiel pacheco <yankielarapacheco@yahoo.com>
2024-02-13 15:27:28 +0100

    Refactoring Todo service for working api-based

--- src/services/todo/todo_services.py
+++ src/services/todo/todo_services.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-import csv
-import os
 
-from src.models import Todo
+from src.entities import TodoEntity
 from src.repository import TodoRepository
 
 
@@ -10,19 +7,11 @@ class TodoService:
     def __init__(self, todo_repository: TodoRepository):
         self.todo_repository = todo_repository
 
-    def save_todo_as_csv(self, todo: Todo, directory="storage"):
-        os.makedirs(directory, exist_ok=True)
-        date_prefix = datetime.now().strftime("%Y_%m_%d")
-        filename = f"{date_prefix}_{todo.id}.csv"
-        filepath = os.path.join(directory, filename)
+    def get_all_todos(self):
+        return self.todo_repository.find_all()
 
-        with open(filepath, 'w', newline='') as csvfile:
-            fieldnames = ['id', 'title', 'completed']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({'id': todo.id, 'title': todo.title, 'completed': todo.completed})
+    def get_all_by_user_id(self, user_id):
+        return self.todo_repository.find_all_by_user_id(user_id)
 
-    def save_todos_as_csv(self, directory="storage"):
-        todos = self.todo_repository.find_all()
-        for todo in todos:
-            self.save_todo_as_csv(todo, directory)
+    def create_todo(self, todo: TodoEntity):
+        return self.todo_repository.create_todo(todo)
